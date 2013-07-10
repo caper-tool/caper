e8cee1e90c5d1750af4f4ad0f6e71e3a92e1a8c6
Pedro da Rocha Pinto <pmd09@doc.ic.ac.uk>
2013-07-10 16:12:52 +0100

    Still working on the Interpreter.Not done at all!

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,54 +1,54 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Interpreter where
 
+import AST
 import Parser
+import Data.Map;
 
-class Eval a where
-  type Out a :: *
-  eval :: a -> Out a
+data Context = Map String Integer
+type State = Map Integer Integer
 
-instance Eval BExpr where
-  type Out BExpr = Bool
-  eval (BoolConst _ b)                  = b
-  eval (Not _ e)                        = not $ eval e
-  eval (BBinary _ And e1 e2)            = (eval e1) && (eval e2)
-  eval (BBinary _ Or e1 e2)             = (eval e1) || (eval e2)
-  eval (RBinary _ Equal e1 e2)          = (eval e1) == (eval e2)
-  eval (RBinary _ NotEqual e1 e2)       = (eval e1) /= (eval e2)
-  eval (RBinary _ Greater e1 e2)        = (eval e1) > (eval e2)
-  eval (RBinary _ Less e1 e2)           = (eval e1) < (eval e2)
-  eval (RBinary _ GreaterOrEqual e1 e2) = (eval e1) >= (eval e2)
-  eval (RBinary _ LessOrEqual e1 e2)    = (eval e1) <= (eval e2)
+class Eval a b where
+  eval :: Context -> a -> b
 
-instance Eval AExpr where
-  type Out AExpr = Integer
-  eval (Var _  n)                 = 0
-  eval (Deref _ e)                = eval e
-  eval (IntConst _ i)             = i
-  eval (Neg _ e)                  = - eval e
-  eval (ABinary _ Add e1 e2)      = eval e1 + eval e2
-  eval (ABinary _ Subtract e1 e2) = eval e1 - eval e2
-  eval (ABinary _ Multiply e1 e2) = eval e1 * eval e2
-  eval (ABinary _ Divide e1 e2)   = quot (eval e1) (eval e2)
-  eval (FunctCall _ n args)       = 0
+instance Eval BExpr Bool where
+  eval ctx (ConstBExpr _ b)                      = b
+  eval ctx (NotBExpr _ e)                        = not (eval ctx e)
+  eval ctx (BinaryBExpr _ And e1 e2)             = (eval ctx e1) && (eval ctx e2)
+  eval ctx (BinaryBExpr _ Or e1 e2)              = (eval ctx e1) || (eval ctx e2)
+--  eval ctx (RBinaryBExpr _ Equal e1 e2)          = (eval ctx e1) == (eval ctx e2)
+--  eval ctx (RBinaryBExpr _ NotEqual e1 e2)       = (eval ctx e1) /= (eval ctx e2)
+--  eval ctx (RBinaryBExpr _ Greater e1 e2)        = (eval ctx e1) > (eval ctx e2)
+--  eval ctx (RBinaryBExpr _ Less e1 e2)           = (eval ctx e1) < (eval ctx e2)
+--  eval ctx (RBinaryBExpr _ GreaterOrEqual e1 e2) = (eval ctx e1) >= (eval ctx e2)
+--  eval ctx (RBinaryBExpr _ LessOrEqual e1 e2)    = (eval ctx e1) <= (eval ctx e2)
 
-instance Eval Stmt where
-  type Out Stmt = (Maybe Integer)
-  eval (StmtSeq _ seq)     = Nothing
-  eval (VarDeclr _ vars)   = Nothing
-  eval (Assign _ e1 e2)    = Nothing
-  eval (ExprStmt _ e)      = Nothing
-  eval (If _ b s1 s2)      = if eval b then eval s1 else eval s2
-  eval (While _ e s)       = Nothing
-  eval (DoWhile _ s e)     = Nothing
-  eval (Return _ Nothing)  = Nothing
-  eval (Return _ (Just e)) = Nothing
-  eval (Break _)           = Nothing
-  eval (Continue _)        = Nothing
-  eval (Skip _)            = Nothing
+instance Eval AExpr Integer where
+  eval ctx (VarAExpr _  n)                = 0
+  eval ctx (DerefAExpr _ e)               = eval ctx e
+  eval _ (ConstAExpr _ i)                 = i
+  eval ctx (NegAExpr _ e)                 = - (eval ctx e)
+  eval ctx (BinaryAExpr _ Add e1 e2)      = (eval ctx e1) + (eval ctx e2)
+  eval ctx (BinaryAExpr _ Subtract e1 e2) = (eval ctx e1) - (eval ctx e2)
+  eval ctx (BinaryAExpr _ Multiply e1 e2) = (eval ctx e1) * (eval ctx e2)
+  eval ctx (BinaryAExpr _ Divide e1 e2)   = quot (eval ctx e1) (eval ctx e2)
+  eval ctx (CallAExpr _ n args)           = 0
+
+--instance Eval Stmt Integer where
+--  eval ctx (SeqStmt _ seq)         = 0
+--  eval ctx (VarStmt _ vars)        = 0
+--  eval ctx (AssignStmt _ e1 e2)    = 0
+--  eval ctx (ExprStmt _ e)          = 0
+--  eval ctx (IfElseStmt_ b s1 s2)   = if (eval ctx b) then (eval ctx s1) else (eval ctx s2)
+--  eval ctx (WhileStmt _ e s)       = 0
+--  eval ctx (DoWhileStmt _ s e)     = 0
+--  eval ctx (ReturnStmt _ Nothing)  = 0
+--  eval ctx (ReturnStmt _ (Just e)) = 0
+--  eval ctx (SkipStmt _)            = 0
 
 --instance Eval Declr
 --  type Out Declr = Maybe Integer
