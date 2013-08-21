6ae9d3bb11ccc1819d0af0c17afcc68543b6feb5
Thomas Dinsdale-Young <td202@doc.ic.ac.uk>
2013-08-21 20:23:00 +0100

    Functioning DPLL

--- DPLL2.hs
+++ DPLL2.hs
@@ -1,13 +1,19 @@
+{-# LANGUAGE RankNTypes #-}
+module DPLL2 where
 import Data.Bits
 import qualified Data.Sequence as Seq
 import Data.Sequence ((|>), (<|), (><), Seq)
-import Control.Monad.State
 import Data.Foldable
 import Prelude hiding (foldl, foldr, mapM, mapM_)
 import Data.Maybe
-import Debug.Trace
 import Control.Monad.State hiding (mapM, mapM_)
 import Data.Traversable
+--import Debug.Trace
+import Math.NumberTheory.Logarithms
+
+trace :: String -> a -> a
+{-# INLINE trace #-}
+trace x y = y
 
 newtype Model = Model (Integer, Integer)
 
@@ -28,6 +34,10 @@ instance Show Clause where
                                 if testBit cx 0 then '+' else '-'
                                 else '*') : show' (shift cx (-1)) (shift mx (-1))
 
+instance Show Model where
+        show (Model (c, m)) = show (Clause (c, m))
+
+
 triSat :: Model -> Clause -> Maybe Bool
 -- Returns Just True if the clause is satisfied
 -- Returns Just False if its negation is satisfied
@@ -36,6 +46,14 @@ triSat (Model (m,mmask)) (Clause (c,cmask))
         | complement (m `xor` c) .&. (mmask .&. cmask) /= 0 = Just True
         | (complement mmask) .&. cmask /= 0 = Nothing
         | otherwise = Just False
+
+validate :: Model -> [Clause] -> Bool
+validate m cs = foldr (\c -> ((Just True == triSat m c) &&)) True cs
+
+watch :: Model -> [Clause] -> a -> a
+watch m cs = foldr (\c -> (if (Just False == triSat m c) then error $ "Model violation\n c:" ++ show c ++ "\n m:" ++ show m else id)) id cs
+
+watch' cs m = watch m cs
 
 propagateUnits :: Model -> Seq Clause -> Model
 propagateUnits m s = propagateUnits m (Seq.filter (\x -> Nothing == triSat m x) s)
@@ -53,29 +71,56 @@ propagateUnits' m s = execState (mapM_ pu s) m
                         else
                                 return ()
 
-dpll' :: Model -> [Clause] -> [Clause] -> Maybe Model
-dpll' m [] [] = return m
-dpll' mdl@(Model (m,mmask)) us (cl@(Clause (c,cmask)) : cs)
-        | complement (m `xor` c) .&. (mmask .&. cmask) /= 0 = dpll' mdl us cs
+dpll :: [Clause] -> Maybe Model
+dpll cs = let m = Model (0,0) in dpll' (\x -> id) (purify m cs) [] cs
+
+dpll' :: (forall a. Model -> a -> a) -> Model -> [Clause] -> [Clause] -> Maybe Model
+dpll' _ m [] [] = return m
+dpll' f mdl@(Model (m,mmask)) us (cl@(Clause (c,cmask)) : cs)
+        | complement (m `xor` c) .&. (mmask .&. cmask) /= 0 = (trace $ "dropping\n c:" ++ show cl ++ "\n m:" ++ show mdl) $ (f mdl) $ dpll' f mdl us cs
         | (complement mmask) .&. cmask /= 0 = let pcm = (complement mmask) .&. cmask in if popCount pcm == 1 then
                                         -- propagate the unit!
-                                        dpll' (Model (m .|. (c .&. pcm), mmask .|. pcm)) [] $! (us ++ cs)
+                                        -- Need to check if propagation forces backtracking!
+                                        (trace $ "propagating: " ++ show (integerLog2' pcm) ++ "\n m:" ++ show mdl ++ "\n c:" ++ show cl) $ (f $ (Model (m .|. (c .&. pcm), mmask .|. pcm))) $ dpll' f (purify (Model (m .|. (c .&. pcm), mmask .|. pcm)) (us++cs)) [] $! (us ++ cs)
                                 else
-                                        dpll' mdl (cl : us) cs
-        | otherwise = mzero
-dpll' m us [] = dpll' mp [] us `mplus` dpll' mn [] us
+                                        dpll' f mdl (cl : us) cs
+        | otherwise = trace "backtracking" mzero
+dpll' f m us [] = (watch m us) $ dpll' f (purify mp us) [] us `mplus` dpll' f (purify mn us) [] us
         where
-                m' = purify m us 0 0 in
-                (mp, mn) = chooseLiteral m' us
+                (mp, mn) = chooseLiteral m us
 
-purify :: Model -> [Clause] -> Integer -> Integer -> Model
-purify (Model (m,mmask)) [] pos neg = Model (m .|. (pos .&. (pos `xor` neg)), mmask .|. (pos `xor` neg))
-purify m ((Clause (c,cm)):cs) pos neg = purify m cs (pos .|. (c .&. cm)) (neg .|. (complement c .&. cm))
+purify m cs = m' -- (trace $ "Purify: " ++ show m ++ " ==> " ++ show m' ++ "\nGiven:\n" ++ show cs) m'
+        where m' = purify' m cs 0 0
+
+purify' :: Model -> [Clause] -> Integer -> Integer -> Model
+purify' (Model (m,mmask)) [] pos neg = Model (m .|. ((complement mmask .&. pos) .&. (pos `xor` neg)), mmask .|. (pos `xor` neg))
+purify' m ((Clause (c,cm)):cs) pos neg = purify' m cs (pos .|. (c .&. cm)) (neg .|. (complement c .&. cm))
 
 chooseLiteral :: Model -> [Clause] -> (Model, Model)
 -- Pick a litral that 
+chooseLiteral m@(Model (mv, mm)) cs = (trace $ "choosing: " ++ show lit) (Model (setBit mv lit, setBit mm lit), Model (clearBit mv lit, setBit mm lit))
+        where
+                lit = (trace $ show m) getMax (rankCommon m cs) (candidateLiterals m cs)
 
 
+getMax :: Ord b => (a -> b) -> [a] -> a
+getMax _ [] = error "getMax called with empty list"
+getMax r (l : ls) = gm l (r l) ls
+        where
+                gm best _ [] = best
+                gm best bestrank (x : xs) = if r x > bestrank then gm x (r x) xs else gm best bestrank xs
+
+candidateLiterals :: Model -> [Clause] -> [Int]
+candidateLiterals (Model (_,mmask)) cs = getBits 0 [] $ complement mmask .&. foldr (\(Clause (_,cm)) -> (cm .|.)) 0 cs
+        where
+                getBits x l 0 = l
+                getBits x l n = getBits (x+1) (if n `testBit` 0 then x : l else l) (shiftR n 1)
+
+rankCommon :: Model -> [Clause] -> Int -> Int
+rankCommon _ cs b = rc cs 0
+        where
+                rc [] n = n
+                rc ((Clause (cm, _)) : cr) n = rc cr $! (if testBit cm b then n + 1 else n)
 
 
 data Proposition a = Prop a
