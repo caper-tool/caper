2d369ac0077258cf99cc5190bafb7ea56eef4e05
Kristoffer Just Andersen <kja@cs.au.dk>
2015-10-05 20:26:55 +0200

    Cleaned PMaybe, added AMP instances

--- src/Caper/Utils/PMaybe.hs
+++ src/Caper/Utils/PMaybe.hs
@@ -1,14 +1,12 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Caper.Utils.PMaybe where
-import Control.Exception
-import Control.Concurrent
-import Control.Concurrent.MVar
+import           Control.Applicative
+import           Control.Concurrent
 import qualified Control.Concurrent.MSemN as Sem
-import Control.Monad.Trans.Maybe
-import Data.Typeable
-import Control.Monad
-import Debug.Trace
-import Prelude hiding (catch)
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.Trans.Maybe
+import           Data.Typeable
 
 newtype PMaybe a = PMaybe {
                 runPMaybe :: Sem.MSemN Int -> MaybeT IO a
@@ -23,16 +21,23 @@ doRunPMaybe pm = do
         s <- Sem.new (threads - 1)
         runMaybeT $ runPMaybe pm s
 
-
 instance Functor PMaybe where
         fmap f c = PMaybe $ fmap f . runPMaybe c
+
+instance Applicative PMaybe where
+        pure a = PMaybe $ \_ -> return a
+        pf <*> pa = PMaybe $ \s -> runPMaybe pf s <*> runPMaybe pa s
 
 instance Monad PMaybe where
         fail m = PMaybe $ \_ -> fail m
         c >>= f = PMaybe $ \s -> do
                                 r1 <- runPMaybe c s
                                 runPMaybe (f r1) s
-        return r = PMaybe $ \_ -> return r
+        return = pure
+
+instance Alternative PMaybe where
+         empty = mzero
+         (<|>) = mplus
 
 instance MonadPlus PMaybe where
         mzero = fail ""
@@ -41,8 +46,8 @@ instance MonadPlus PMaybe where
                 if b then do
                         relmv <- newEmptyMVar
                         let release = do
-                                b <- tryPutMVar relmv ()
-                                when b $ Sem.signal s 1
+                                successful <- tryPutMVar relmv ()
+                                when successful $ Sem.signal s 1
                         answermv <- newEmptyMVar
                         child1 <- forkIO $ child c1 answermv s
                         child2 <- forkIO $ child c2 answermv s
@@ -68,14 +73,3 @@ instance MonadPlus PMaybe where
                         putMVar mv res') `catch` (\ResultUnneededException -> return ())
                 evalMaybe (Just x) = x `seq` Just x
                 evalMaybe Nothing = Nothing
-                tlog m = do
-                        tid <- myThreadId
-                        putStrLn $ show tid ++ "> " ++ m 
-
-{-
-foo n = return $ head $ filter (\x -> x `mod` n == 0) [1..]
-
-main = do
-        res <- doRunPMaybe (foo 1000111111 `mplus` foo 101010101)
-        print res --(foo 100011111 `mplus` foo 101010101 :: Maybe Integer)
-        -}
