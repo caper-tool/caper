5443a9d4a53708586b2fe0eb8c951fa2b99e1ab6
Pedro da Rocha Pinto <pmd09@doc.ic.ac.uk>
2015-11-08 22:31:31 +0000

    Added automated testing.

---
+++ Tests/RunTests.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+import Control.Applicative
+import Data.List(sort)
+import Control.Monad (forM_)
+import Shelly
+import Prelude hiding (FilePath, lines, drop, concat)
+import System.Directory
+import Data.Text hiding (last, length)
+import System.FilePath
+import System.Environment
+
+caperPath = "dist/build/Caper/Caper"
+testsPath = "runtests/"
+rootPath = "../../../"
+
+testFile name = do
+    absCaperPath   <- absPath caperPath
+    absName        <- absPath name
+    contents       <- readfile absName
+    contentsLines  <- return $ lines contents
+    testName       <- return (drop 9 $ contentsLines !! 0)
+    expectedResult <- return $ concat $ (drop 11 $ contentsLines !! 1):["ED"]
+    out            <- run absCaperPath [toTextIgnore absName]
+    exitCode       <- lastExitCode
+    echo $ concat $ [testName, ": ", if exitCode == 0 && expectedResult == (last $ lines out)
+               then "\ESC[32m\STXPASSED\ESC[m\STX"
+               else "\ESC[31m\STXFAILED\ESC[m\STX"]
+
+main = shelly $ silently $ errExit False $ do
+    dir   <- liftIO $ getExecutablePath
+    cd $ fromText $ pack $ takeDirectory $ dir
+    cd $ rootPath
+    value <- pwd
+    names <- findWhen (\n -> return $ isSuffixOf ".t" $ toTextIgnore n) testsPath
+    forM_ names $ \n -> do
+        testFile n
